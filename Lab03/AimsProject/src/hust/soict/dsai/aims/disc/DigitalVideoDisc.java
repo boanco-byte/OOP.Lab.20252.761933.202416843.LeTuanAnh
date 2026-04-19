04e899c844110c943449c138a09057e2cacd4d2b
Tuan Anh <letuananh04062006@gmail.com>
2026-04-19 16:43:20 +0700

    Hoàn thành bài tập Lab03

--- Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -1,3 +1,4 @@
+package hust.soict.dsai.aims.disc;
 
 public class DigitalVideoDisc {
 	
@@ -7,6 +8,8 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	private int id;
+	private static int nbDigitalVideoDiscs = 0;
 	
 	public static void main(String[] args) {
 		
@@ -16,12 +19,16 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
         super();
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String director, String category, String title, float cost) {
@@ -29,6 +36,8 @@ public class DigitalVideoDisc {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -38,6 +47,19 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
+	}
+	
+	public String toString() {
+		return "DVD - " + this.getTitle() + " - "
+				+ this.getCategory() + " - "
+				+ this.getDirector() + " - "
+				+ this.getLength() + ": " + this.getCost();
+	}
+	
+	public boolean isMatch(String title) {
+	    return this.title.toLowerCase().contains(title.toLowerCase());
 	}
 	
 	public String getTitle() {
@@ -58,6 +80,34 @@ public class DigitalVideoDisc {
 
 	public float getCost() {
 		return cost;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
---
+++ Lab03/OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -0,0 +1,28 @@
+package hust.soict.dsai.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoGarbage {
+    public static void main(String[] args) throws Exception {
+
+        String filename = "C:/OOP/OOP.Lab.20252.761933.202416843.LeTuanAnh/Lab03/OtherProjects/src/hust/soict/dsai/test.exe";
+        
+        try {
+            byte[] inputBytes = Files.readAllBytes(Paths.get(filename));
+            
+            long startTime = System.currentTimeMillis();
+            StringBuilder outputStringBuilder = new StringBuilder();
+            for (byte b : inputBytes) {
+                outputStringBuilder.append((char)b);
+            }
+            String outputString = outputStringBuilder.toString();
+            long endTime = System.currentTimeMillis();
+            
+            System.out.println("Processing time (StringBuilder): " + (endTime - startTime) + "ms");
+        } catch (Exception e) {
+            System.err.println("Lỗi: Không tìm thấy file! Hãy kiểm tra lại đường dẫn.");
+            e.printStackTrace();
+        }
+    }
+}
